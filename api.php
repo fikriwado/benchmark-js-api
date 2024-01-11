585dd2fc6b5c0fda2cbc4c4b5d1408b8b028d4ad
Moch Khoirurrizal <moch.khoirurrizal@gdn-commerce.com>
2024-01-11 21:46:53 +0700

    update header access control

--- api.php
+++ api.php
@@ -3,6 +3,8 @@ require_once __DIR__ . '/functions/config.php';
 require_once __DIR__ . '/functions/model.php';
 
 header('Content-Type: application/json');
+header('Access-Control-Allow-Headers: *');
+header('Access-Control-Allow-Origin: *');
 
 if (isset($_GET['limit']) && !empty($_GET['limit'])) {
   $api = new Users;
